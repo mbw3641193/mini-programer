4298048a687d8f8ace1d84a2b508a951a58335b1
mbw3641193 <305683054@qq.com>
2019-08-06 00:11:47 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,49 @@
+//index.js
+//获取应用实例
+
+
+Page({
+  data: {
+    background: ['../../images/index-png_16.png', '../../images/index-png_16.png', '../../images/index-png_16.png'],
+    needData:[
+      {
+        title:'铝合金车轮轻铝合金车轮轻量技术量技术',
+        price:20,
+        year:2,
+        label: ['金属材料', '铸造工艺']
+      },
+      {
+        title: '铝合金车轮轻量技术',
+        price: 20,
+        year: 2,
+        label: ['金属材料', '铸造工艺']
+      },
+      {
+        title: '铝合金车轮轻量技术',
+        price: 20,
+        year: 2,
+        label: ['金属材料', '铸造工艺']
+      },
+      {
+        title: '铝合金车轮轻量技术',
+        price: 20,
+        year: 2,
+        label: ['金属材料', '铸造工艺']
+      }
+    ]
+    
+    
+  },
+  //事件处理函数
+  gotoUrl: function(e) {
+    
+    var src =  e.currentTarget.dataset.src;
+    wx.navigateTo({
+      url: src
+    })
+  },
+  onLoad: function () {
+    
+  },
+  
+})
